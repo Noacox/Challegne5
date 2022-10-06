017f9eb36cc371af60ccbffb9d6e55fac3d1ba8a
NoaCox <noacox24@gmail.com>
2022-10-05 22:16:43 -0500

    almost spring3

--- src/main/java/com/usa/misiontic/reto3c3/controller/CloudController.java
+++ src/main/java/com/usa/misiontic/reto3c3/controller/CloudController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/cloud")
+@RequestMapping("/api/cloud")
 public class CloudController {
     @Autowired
     private CloudService cloudService;
